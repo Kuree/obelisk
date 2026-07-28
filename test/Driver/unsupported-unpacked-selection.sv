3a9c983b81b5803230e589014719a5648d34f2df
Keyi Zhang <keyi@cs.stanford.edu>
2026-07-27 22:35:56 -0700

    Update unpacked slice driver expectation

--- test/Driver/unsupported-unpacked-selection.sv
+++ test/Driver/unsupported-unpacked-selection.sv
@@ -1,4 +1,4 @@
-// RUN: not obelisk -O0 %s -o %t 2>&1 | FileCheck %s
+// RUN: obelisk -O0 %s -o %t
 
 module unsupported_unpacked_selection;
   typedef struct {
@@ -10,7 +10,4 @@ module unsupported_unpacked_selection;
   initial value.elements[3:1] = '{32'd4, 32'd3, 32'd2};
 endmodule
 
-// Unpacked aggregate ranges are retained as first-class values. Until slice
-// assignment is implemented, reject this path before asking packed-width
-// helpers to inspect a non-packed result type.
-// CHECK: error: unsupported semantic node in the first simulation slice: obelisk.sv.expression.range_select (selection result type)
+// Unpacked aggregate slice assignments are lowered element by element.
