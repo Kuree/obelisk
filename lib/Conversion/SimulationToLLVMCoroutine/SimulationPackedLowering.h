f679f08866918dade2a0b421425c4ac3ed100163
Keyi Zhang <keyi@cs.stanford.edu>
2026-07-30 00:05:38 -0700

    Extract packed simulation pattern lowering

---
+++ lib/Conversion/SimulationToLLVMCoroutine/SimulationPackedLowering.h
@@ -0,0 +1,19 @@
+//===- SimulationPackedLowering.h - Packed simulation conversion ------===//
+
+#ifndef OBELISK_LIB_CONVERSION_SIMULATIONTOLLVMCOROUTINE_SIMULATIONPACKEDLOWERING_H
+#define OBELISK_LIB_CONVERSION_SIMULATIONTOLLVMCOROUTINE_SIMULATIONPACKEDLOWERING_H
+
+#include "SimulationNBALowering.h"
+#include "SimulationToLLVMCoroutinePrivate.h"
+
+namespace obelisk::detail {
+
+mlir::LogicalResult lowerPackedSimulationOperations(
+    mlir::ModuleOp module, const llvm::DataLayout &dataLayout,
+    const NativeStateLayout &stateLayout, bool enableDirectStaticState,
+    const NativeStaticNBAPlan *staticNBAPlan, bool vpiAllowsWrite);
+
+} // namespace obelisk::detail
+
+#endif // OBELISK_LIB_CONVERSION_SIMULATIONTOLLVMCOROUTINE_SIMULATIONPACKEDLOWERING_H
+
